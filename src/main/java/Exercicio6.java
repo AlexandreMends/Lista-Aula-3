c98076000fb2c77f0acef805404de83cb1a104c1
Alexandre mendes <ale.alexandremendes@hotmail.com>
2022-03-04 23:07:22 -0300

    Exercicio

--- src/main/java/Exercicio6.java
+++ src/main/java/Exercicio6.java
@@ -1,21 +1,22 @@
 import java.util.Random;
 import java.util.Scanner;
-
-public class Exercicio5 {
+public class Exercicio6 {
     public static void main(String[] args) {
         //Objeto tipo Scanner
-            Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         //Randon para gerar numero aleatorio
-            Random rand = new Random();
-            int numGerado, numAdivinhado;
+        Random rand = new Random();
+        int numGerado, numAdivinhado;
         //Numero aleatorios de 1 a 10
         numGerado = rand . nextInt(10) +1;
 
         do {
             System.out.println("Qual o numero que estou pensando? ");
             numAdivinhado = entrada.nextInt();
-            if (numAdivinhado != numGerado) {
-                System.out.println("Errrouu");
+            if (numAdivinhado > numGerado) {
+                System.out.println("O numero adivinhado e maior que o seu");
+            }else if (numAdivinhado < numGerado) {
+                System.out.println("O numero adivinhado e menor que o seu");
             }
 
         }while (numGerado !=numAdivinhado);
@@ -26,3 +27,5 @@ public class Exercicio5 {
     }
 
 }
+
+
